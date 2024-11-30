aba0fae78d68e44d93a3c5e688bfa1c7f3c9c48b
DreamFXX <filipek.husak@gmail.com>
2024-11-30 20:53:23 +0100

    added proj. in progress to fork of academy-review

---
+++ CodingTracker_ConsoleApp.DreamFXX/AppManager.cs
@@ -0,0 +1,51 @@
+﻿using Spectre.Console;
+
+class AppManager
+{
+    private const string START_CODE_SESH = "Start recording actual Code compiling session";
+    private const string END_CODE_SESH = "End or stop active Code compiling session(s)";
+    
+    private const string SAVE_ACTIVE_CODETIME_SESH = "Save your newly recorded Coding sessions";
+    private const string VIEW_ALL_STORED_SESSIONS = "View all stored sessions and coding times";
+    private const string DELETE_CODE_SESH_DATA = "Delete your already saved Coding sessions and logs";
+
+    private const string EXIT = "Close Application";
+
+    private DatabaseManager dDatabaseManager;
+    public AppManager(DatabaseManager databaseManager)
+    {
+        dDatabaseManager = databaseManager;
+    }
+
+    public void AppStart()
+    {
+        while (true)
+        {
+            MainMenu();
+        }
+        
+    }
+
+    private void MainMenu()
+    {
+        AnsiConsole.Clear();
+
+        List<string> menuRoutes = new List<string>
+        {
+            START_CODE_SESH,
+            END_CODE_SESH,
+            SAVE_ACTIVE_CODETIME_SESH,
+
+            VIEW_ALL_STORED_SESSIONS,
+            DELETE_CODE_SESH_DATA,
+
+            EXIT,
+        };    
+    }
+
+    private void menuRouter()
+    {
+        throw new NotImplementedException();
+    }
+}
+
---
+++ CodingTracker_ConsoleApp.DreamFXX/DatabaseManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Data;
+using System.Data.SQLite;
+using Dapper;
+
+class DatabaseManager
+{
+    private string _connectionString = "";
+
+    public DatabaseManager(string connectionString)
+    {
+        _connectionString = connectionString;
+        SqlMapper.AddTypeHandler(new TimeSpanHandler());
+
+        using (SQLiteConnection conn = new SQLiteConnection(_connectionString))
+        {
+            conn.Execute("CREATE TABLE IF NOT EXISTS Codingtime_Data (Id INTEGER PRIMARY KEY AUTOINCREMENT, Name TEXT, TimeStarted TEXT, TimeEnded TEXT, Duration INT, ActiveStat INTEGER DEFAULT 1)");
+        }
+    }
+}
+
+
+class TimeSpanHandler : SqlMapper.TypeHandler<TimeSpan>
+{
+    public override TimeSpan Parse(object value)
+    {
+        Console.WriteLine(value);
+        return TimeSpan.FromTicks((int)value);
+    }
+
+    public override void SetValue(IDbDataParameter parameter, TimeSpan value)
+    {
+        parameter.Value = value.Ticks;
+    }
+}
