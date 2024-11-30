12b6a15f43d29bec0472a536e4ce7222aa38f04a
DreamFXX <filipek.husak@gmail.com>
2024-11-30 21:48:13 +0100

    Main menu and routing done

--- CodingTracker_ConsoleApp.DreamFXX/AppManager.cs
+++ CodingTracker_ConsoleApp.DreamFXX/AppManager.cs
@@ -7,6 +7,7 @@ class AppManager
     
     private const string SAVE_ACTIVE_CODETIME_SESH = "Save your newly recorded Coding sessions";
     private const string VIEW_ALL_STORED_SESSIONS = "View all stored sessions and coding times";
+    private const string CHANGE_TIME_IN_SESSION = "";
     private const string DELETE_CODE_SESH_DATA = "Delete your already saved Coding sessions and logs";
 
     private const string EXIT = "Close Application";
@@ -37,13 +38,85 @@ class AppManager
             SAVE_ACTIVE_CODETIME_SESH,
 
             VIEW_ALL_STORED_SESSIONS,
+            CHANGE_TIME_IN_SESSION,
             DELETE_CODE_SESH_DATA,
 
             EXIT,
-        };    
+        };
+
+        var selected = AnsiConsole.Prompt(
+            new SelectionPrompt<string>()
+            .Title("Welcome in Coding Session Tracker - Log your spent time with Coding!")
+            .PageSize(9)
+            .MoreChoicesText("[grey](Move with arrows, up or down to show more options.)[/]")
+            .AddChoices(menuRoutes)
+            );
+        try
+        {
+            MenuRouter(selected);
+        }
+        catch (Exception e)
+        {
+            AnsiConsole.MarkupLine($"[red]An error has been detected. Restart or fix your application, read an error message below.[/]\n\n[red]{e.Message}[/]");
+            Console.ReadKey();
+        }
     }
 
-    private void menuRouter()
+    private void MenuRouter(string selected)
+    {
+        switch (selected)
+        {
+            case START_CODE_SESH:
+                StartSession();
+                break;
+            case END_CODE_SESH:
+                EndSession();
+                break;
+            case SAVE_ACTIVE_CODETIME_SESH:
+                SaveCodeTimeSession();
+                break;
+            case VIEW_ALL_STORED_SESSIONS:
+                ViewAllSessions();
+                break;
+
+            case CHANGE_TIME_IN_SESSION:
+                ChangeCodingSession();
+                break;
+            case DELETE_CODE_SESH_DATA:
+                DeleteCodingSession();
+                break;
+            case EXIT:
+                Environment.Exit(0);
+                break;
+        }
+    }
+
+    private void DeleteCodingSession()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void ChangeCodingSession()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void ViewAllSessions()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void SaveCodeTimeSession()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void EndSession()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void StartSession()
     {
         throw new NotImplementedException();
     }
--- CodingTracker_ConsoleApp.DreamFXX/Data/CodingSession.cs
+++ CodingTracker_ConsoleApp.DreamFXX/Data/CodingSession.cs
@@ -1,16 +1,25 @@
-﻿
-
-class CodingSession
+﻿class CodingSession
 {
     public int Id { get; set; }
 
     public DateTime StartTime { get; set; }
     public DateTime EndTime { get; set; }
-    public DateTime _endTime { get; set; }
+    public DateTime _endTime {  get; set; }
 
     public string? Duration { get; set; }
-
+    public string? Name { get; set; }
     public bool ActiveStat { get; set; }
+
+    public CodingSession()
+    {
+        Name = "Coding Session";
+        ActiveStat = true;
+    }
+    public CodingSession(bool isActive)
+    {
+        Name = "Coding Session";
+        ActiveStat = isActive;
+    }
 }
 
     
