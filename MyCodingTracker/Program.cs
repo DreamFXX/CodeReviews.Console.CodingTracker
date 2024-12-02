c329b5ab68e897571734f0615e1a622a162ce86d
DreamFXX <filipek.husak@gmail.com>
2024-12-02 16:26:04 +0100

    quick txt

---
+++ MyCodingTracker/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace MyCodingTrack
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ProgramController.StartProgram();
+
+            Console.ReadKey();
+        }
+    }
+}
